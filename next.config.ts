46b33dea2986c74b45fd1cc188ebbaa80d1dd158
koooohey <aarrppeeggiioo7@gmail.com>
2025-02-12 22:16:38 +0900

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+	/* config options here */
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "images.microcms-assets.io",
+				pathname: "/assets/**",
+			},
+		], // microCMSのドメインを追加
+	},
 };
 
 export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import "@/styles/globals.css";
+import "modern-css-reset/dist/reset.min.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Noto_Sans_JP } from "next/font/google";
+
+const notoSansJP = Noto_Sans_JP({
+	weight: ["400", "700"], // 必要なウェイトを指定
+	subsets: ["latin"], // 必要なサブセットを指定
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://use.typekit.net/cpu0oyy.css"
+				/>
+			</Head>
+			<Component
+				{...pageProps}
+				notoSansJPClassName={notoSansJP.className}
+			/>
+		</>
+	);
 }
